148c4401f60eb455d61b1d2d4dd9870df2b4931c
e.pottinger <e.pottinger@28B5CS09.sjsd.lcl>
2018-10-18 14:47:34 -0500

    Ethan, i know you have an idea of how you can rotate, i tried it but it didnt work, right now im just going to update it because i have to learn, bye babe.

--- src/tetrisgame/TetrisGame.java
+++ src/tetrisgame/TetrisGame.java
@@ -12,14 +12,6 @@ public class TetrisGame {
     public static void main(String[] args) {
         TetrisBoard cx = new TetrisBoard();
         String title = "yee";
-        for(int i = 19; i >= 7; i--) {
-            cx.activate(new Coordinates(0, i));
-            cx.activate(new Coordinates(1, i));
-            cx.activate(new Coordinates(2, i));
-            cx.activate(new Coordinates(7, i));
-            cx.activate(new Coordinates(8, i));
-            cx.activate(new Coordinates(9, i));
-        }
         cx.outputGrid("ligma");
         cx.spawnTetrimino(cx.randInt(1, 7));
         cx.outputGrid("fugma");
@@ -29,10 +21,9 @@ public class TetrisGame {
         cx.outputGrid("sugondese");
         cx.moveDown();
         cx.outputGrid("bofa");
-        for(int i = 0; i < 10000; i++) {
-            cx.moveDown();
-            cx.outputGrid("steve jobs");
-        }
+        cx.rotate();
+        cx.outputGrid(title);
+        
         
     }
     
